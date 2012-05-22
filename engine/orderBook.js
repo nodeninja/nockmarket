0d51bc19f618d8c36d28c944840d7c3bd6573a14
Don Nguyen <don@unswrc.com>
2012-05-22 23:33:48 +1000

    update

--- engine/orderBook.js
+++ engine/orderBook.js
@@ -1,8 +1,94 @@
+'use strict';
+
+var $ = require('jquery')
+    , BinaryHeap = require('./BinaryHeap');
+
+var BUY = "BUY";
+var SELL = "SELL";
+
 module.exports = {
-    add: function add(orderType, stockCode, price, volume, bookData, callback) {
-        callback(null, null);
+    
+    add: function add(orderType, price, volume, exchangeData, callback) {
+        
+        // Avoid side effects
+        var priceString = price.toString();
+        if (SELL === orderType) {
+            priceString = (-price).toString();
+        }
+        
+        // Clone to avoid side effects
+        var clonedVolumes = {};
+        if (exchangeData.volumes)
+            clonedVolumes = $.extend({}, exchangeData.volumes); 
+            
+        var oldVolume = clonedVolumes[priceString];
+
+        // Store order prices
+        var clonedBuys; 
+        if (exchangeData.buys)
+            clonedBuys = $.extend({}, exchangeData.buys); 
+        else
+            clonedBuys = createBinaryHeap();
+            
+        var clonedSells;
+        if (exchangeData.sells)
+            clonedSells = $.extend({}, exchangeData.sells); 
+        else
+            clonedSells = createBinaryHeap();            
+        
+        // We only need to store prices if they are new otherwise we get 
+        // duplicate prices
+        if (oldVolume) {            
+            if (SELL === orderType) {
+                clonedSells.push(-price);
+            }
+            else {
+                clonedBuys.push(price);
+            }
+        }
+        
+        // Was there a trade? This occurs when a buy order matches with a sell 
+        // order or vice versa
+        var trade = false;
+        if (BUY === orderType && 
+            clonedSells.length > 0 && 
+            price >= Math.abs(clonedSells.peek())) {
+            trade = true;
+        }
+        else if (SELL === orderType && 
+                    clonedBuys.length > 0 && 
+                    price <= clonedBuys.peek()) {
+            trade = true;
+        }
+        
+        // A trade means several things
+        // 1. The existing order is not added to the book
+        // 2. Matching orders on the other side of the book are wiped out
+        // 3. Trades are returned in an array
+        if (trade) {
+            var remainingVolume = volume;
+            while (remainingVolume > 0) {
+                if (BUY === orderType) {
+                    var bestSellPrice = clonedBuys.peek();
+                    var bestSellVolume = 0;
+                }
+            }
+        }
+
+        var newVolume = volume;
+        
+        // Add to existing volume
+        if (oldVolume) newVolume += oldVolume;
+        clonedVolumes[priceString] = newVolume;
+        
+        callback(null, {volumes: clonedVolumes, buys: clonedBuys, sells: clonedSells});
+        
     },
 
-    BUY: "BUY",
-    SELL: "SELL"
+    BUY: BUY,
+    SELL: SELL
+}
+
+function createBinaryHeap() {
+    return new BinaryHeap(function(x){return x;});
 }
--- test/orderBook.test.js
+++ test/orderBook.test.js
@@ -6,12 +6,34 @@ var should = require('should');
 
 suite('orderBook', function() {
     
-    var bookData = {};
-    test('addOrder should add a nockmarket order', function(done) {
-        orderBook.add(orderBook.BUY, "NOCK1", 10.5, 1000, bookData, function(err, book) {
+    var exchangeData = {};
+    test('addOrder should add a BUY nockmarket order', function(done) {
+        orderBook.add(orderBook.BUY, 10.5, 1000, exchangeData, function(err, newData) {
             should.not.exist(err);
+            newData.volumes['10.5'].should.eql(1000);
+            exchangeData = newData;
             done();
         });        
-    });   
+    }); 
+    
+    test('addOrder should add a SELL nockmarket order', function(done) {
+        orderBook.add(orderBook.SELL, 11, 750, exchangeData, function(err, newData) {
+            should.not.exist(err);
+            newData.volumes['-11'].should.eql(750);
+            exchangeData = newData;
+            done();
+        });        
+    });     
+
+    test('addOrder should cumulate BUY orders', function(done) {
+        orderBook.add(orderBook.BUY, 10.5, 500, exchangeData, function(err, newData) {
+            should.not.exist(err);
+            newData.volumes['10.5'].should.eql(1500);
+            exchangeData = newData;
+            while (exchangeData.buys.size() > 0)
+                console.log(exchangeData.buys.pop());
+            done();
+        });        
+    });  
 
 });
